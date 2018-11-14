c3c7f93fd8a0f299db2fd7da01780b64b19d4a21
Herlevsen9 <44832544+Herlevsen9@users.noreply.github.com>
2018-11-14 12:39:36 +0100

    Create Opret-HyperV.ps1

---
+++ Opret-HyperV.ps1
@@ -0,0 +1,7 @@
+# Aktiver Hyper feature med services og tools
+Install-WindowsFeature -Name Hyper-V -IncludeManagementTools -Restart
+
+Get-NetAdapter 
+
+# Opret ekstern vSwitch
+New-VMSwitch -name ExternalSwitch  -NetAdapterName Ethernet -AllowManagementOS $true  
