54280d1d12a96c1b3c845d29110b1fdb653f0b19
Herlevsen9 <44832544+Herlevsen9@users.noreply.github.com>
2018-12-03 00:31:58 +0100

    Update Indsæt-disk.ps1

--- Indsæt-disk.ps1
+++ Indsæt-disk.ps1
@@ -1,5 +1,24 @@
 # Bruges til fx. Filserver når der indsættes en datadisk
 
+# Mulige variabler
+[Parameter(Mandatory=$true,
+Position=0)]
+$VMnavn,
+[Parameter(Mandatory=$true,
+Position=1)]
+$HDDstørrelse,
+[Parameter(Mandatory=$true,
+Position=2)]
+$Filsystemlabel,
+[Parameter(Mandatory=$false)]
+$Filformat="NTFS",
+[Parameter(Mandatory=$false)]
+$Drevbogstav
+
+# Opret ny VHDX, sæt størrelse (fx. 100GB)
+
+# Tilføj til VM
+
 # Finder alle diske, der står til "raw"
 Get-Disk | Where partitionstyle -eq 'raw' |
 
@@ -7,4 +26,4 @@ Initialize-Disk -PartitionStyle MBR -PassThru |
 
 New-Partition -AssignDriveLetter -UseMaximumSize |
 
-Format-Volume -FileSystem NTFS -NewFileSystemLabel "disk2" -Confirm:$false
+Format-Volume -FileSystem $Filformat -NewFileSystemLabel $Filsystemlabel -Confirm:$false
