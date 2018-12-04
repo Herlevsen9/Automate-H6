21f4f6ec239ba8bc886302abb62c4982efd3e286
Herlevsen9 <44832544+Herlevsen9@users.noreply.github.com>
2018-12-04 22:59:17 +0100

    Update Opret-Goldenimage.ps1

--- Opret-Goldenimage.ps1
+++ Opret-Goldenimage.ps1
@@ -1,7 +1,5 @@
 # script til at oprette MASTER/Goldenimage VM, der syspreppes og andre VM'er bliver oprettet ud fra
 
-# Link til Lab oprettelse: https://blogs.msmvps.com/russel/category/hyper-v/
-
 # Link til DVD drive https://mcpmag.com/articles/2017/03/09/creating-a-vm-in-hyperv-using-ps.aspx
 # Efter oprettele, følg guide for at kontrollere indstillingerne er sat, Link: https://www.vembu.com/blog/automating-hyper-v-virtual-machine-deployment-powershell/
 
@@ -19,27 +17,39 @@ New-VM -Name $VMnavn -Path $Sti  -MemoryStartupBytes $RAMstart  -Generation 2 -N
 
 # Sæt CPU
 set-vm -Name $VMnavn -ProcessorCount $Cpuantal -DynamicMemory -MemoryStartupBytes $RAMstart `
--MemoryMinimumBytes $RAMminimum -MemoryMaximumBytes $RAMmax
+-MemoryMinimumBytes $RAMminimum -MemoryMaximumBytes $RAMmax -
 
 # kontroller indstillingerne
 
-#
-# Mangler at indsætte et DVD drev
-#
+# Tilføj DVD drev med ISO
+Add-VMDvdDrive -VMName $VMnavn -Path $DVDsti
 
-# Indsæt ISO i DVD drev
-Set-VMDvdDrive -VMName $VMnavn -Path $DVDsti
 
 # Find syntaks i denne guide: http://itproctology.blogspot.com/2013/10/modifying-hyper-v-generation2-vm-boot.html
-# Sæt VM til at boote fra DVD
-Set-VMFirmware $VMnavn -FirstBootDevice 
+
+$VMNetværksadapter = Get-VMNetworkAdapter -VMName $VMnavn
+
+$VMHarddisk = Get-VMHardDiskDrive -VMName $VMnavn
+
+$VMDvddrev = Get-VMDvdDrive -VMName $VMnavn
+
+# Sæt VM's bootorder til HDD, dernæst netværksadapter
+Set-VMFirmware -VMname $VMnavn -BootOrder $VMHarddisk, $VMDvddrev, $VMNetværksadapter
+
+Get-VMFirmware $VMnavn | select * | Sort-Object
+
+# Sæt VM til at boote fra DVD første gang
+Set-VMFirmware -VMname $VMnavn -FirstBootDevice $VMDvddrev
+
+# Færdiggør installation, tryk på en vilkårlig tast for acceptere boot fra DVD
+vmconnect.exe $env:COMPUTERNAME $VMnavn 
+
+Start-Sleep -Seconds 5
 
 # Start VM
 Start-VM $VMnavn
 
-# Færdiggør installation
-
-# Sluk VM
+# Sluk VM efter endt installation
 Stop-VM $VMnavn
 
 # Overfør sysprep fil
