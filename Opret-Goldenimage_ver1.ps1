5e6511ab17a42cc132187d4f4e771cc63e6f305e
Herlevsen9 <44832544+Herlevsen9@users.noreply.github.com>
2018-12-16 23:54:40 +0100

    Update Opret-Goldenimage_ver1.ps1

--- Opret-Goldenimage_ver1.ps1
+++ Opret-Goldenimage_ver1.ps1
@@ -1,5 +1,6 @@
 # script til at oprette MASTER/Goldenimage VM, der syspreppes og andre VM'er bliver oprettet ud fra
 
+# Link til DVD drive https://mcpmag.com/articles/2017/03/09/creating-a-vm-in-hyperv-using-ps.aspx
 # Efter oprettele, følg guide for at kontrollere indstillingerne er sat, Link: https://www.vembu.com/blog/automating-hyper-v-virtual-machine-deployment-powershell/
 
 $VMnavn = "Goldenimage"
@@ -11,6 +12,11 @@ $RAMminimum = 512MB
 $RAMmax = 4GB
 $Cpuantal=2
 $DVDsti = "$env:SystemDrive\iso\2016_x64_EN_Eval.iso"
+$UnattendXML_sti = "$env:SystemDrive\Install\Scripts\autounattend.xml"
+$VM_Lokaladministrator = "$VMnavn\Administrator"
+$VM_Lokal_kodeord = ConvertTo-SecureString -String 'Pa$$w0rd' -AsPlainText -Force
+$VMlokalcredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $VM_Lokaladministrator, $VM_Lokal_kodeord
+
 
 New-VM -Name $VMnavn -Path $Sti  -MemoryStartupBytes $RAMstart  -Generation 2 -NewVHDPath $HDDsti -NewVHDSizeBytes $HDDstørrelse
 
@@ -58,12 +64,39 @@ pause
 # Sluk VM efter endt installation
 Stop-VM $VMnavn
 
-# Overfør sysprep fil
+#######################
+# Overfør sysprep fil #
+#######################
+
+# Hent VM navn
+$VMnavn = Get-VM -Name $VMnavn | Select-Object Name -ExpandProperty name
+
+# Sti til virtuel HDD
+# $VHDsti = Get-childitem -Recurse -Path C:\HYPERV\ | Where-Object {$_.Name -match "$VMnavn.vhdx"} | Select-Object Fullname -ExpandProperty Fullname
+
+# Mount VM virtuel HDD
+Mount-VHD -Path $HDDsti
+
+#Find drev bogstav
+$VolumeDrevbogstav = Get-DISKIMAGE $HDDsti | get-disk | Get-Partition | Get-Volume | ?{$_.FilesystemLabel -ne "Recovery"} | Select-Object Driveletter -Expandproperty DriveLetter
+
+# Destinationssti for filerne
+$Fil_destination = "$VolumeDrevbogstav"+":\"
+
+# Overfør filerne
+Copy-item -Path $UnattendXML_sti -Destination $Fil_destination -force
+
+Dismount-VHD -Path $HDDsti
 
 # Start VM
 Start-VM $VMnavn
 
+# Vent indtil VM er klar til invoke kommandoer
+ while ((Invoke-Command -VMName $VMnavn -Credential $VMlokalcredential {“Test”} -ea SilentlyContinue) -ne “Test”) {Sleep -Seconds 1}
+
 # Kør sysprep kommando med shutdown
+Invoke-Command -VMName $VMnavn -Credential $VMlokalcredential -ScriptBlock {Set-Location c:\Windows\System32\Sysprep
+
+.\sysprep.exe /generalize /oobe /mode:vm /shutdown /unattend:C:\autounattend.xml}
 
 # VM er ny færdig og klar til at blive clonet
-
